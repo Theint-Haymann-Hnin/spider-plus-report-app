a67daa0962ddfa73c9537958e567944c13f23a54
Theint Haymann Hnin <85533704+Theint-Haymann-Hnin@users.noreply.github.com>
2022-01-27 16:34:36 +0630

    add:json file

---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="box col-md-6">
+        <p class="ttl">SPIDER<span class="red-word">PLUS</span></p>
+        <p class="text">Please Select Name to write report.......</p>
+        <select v-model="selected" class="form-select form-control">
+          <option>Select Name</option>
+          <option value="Zaw Khant Maung">Zaw Khant Maung</option>
+          <option value="Min Aung Paing">Min Aung Paing</option>
+          <option value="Theint Haymann Hnin">Theint Haymann Hnin</option>
+        </select>
+        <button type="button" class="btn btn-success mt-5" @click="submit()">
+          Submit
+        </button>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  computed: {},
+  data: {
+    selected: '',
+  },
+
+  methods: {
+    submit() {
+      
+    },
+  },
+}
+</script>
+<style>
+.container .row {
+  margin-top: 50px;
+}
+.container .row .box {
+  border: 3px solid cadetblue;
+  height: 500px;
+  border-radius: 20px;
+}
+.container .ttl {
+  font-size: 50px;
+  font-family: 'Fjalla One', sans-serif;
+  text-align: center;
+  margin-top: 60px;
+  margin-bottom: 20px;
+}
+.container .ttl .red-word {
+  color: #e34234;
+}
+.container .box {
+  width: 500px;
+  margin: 50px auto;
+}
+.text {
+  text-align: center;
+  font-size: 20px;
+  margin-bottom: 50px;
+}
+</style>
---
+++ store/index.js
@@ -0,0 +1,18 @@
+export const namespaced = true
+
+export const state = () => {
+    return {
+        currentreport: null,
+    }
+}
+
+export const mutations = {
+
+}
+
+export const actions = {
+
+}
+export const getters = {
+
+}
